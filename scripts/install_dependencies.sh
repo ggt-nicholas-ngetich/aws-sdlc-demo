b823e1a9bc6a495319c7b52611381d1a5453c2bd
Nicholas Ngetich <nicholas.ngetich@griffinglobaltech.com>
2025-07-15 17:16:08 +0300

    update app

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
+
+# Update system packages and install Python3/pip3 if not present
+yum update -y
+yum install -y python3 python3-pip
+
+# Install Python dependencies
 cd /home/ec2-user/app
 pip3 install -r requirements.txt
