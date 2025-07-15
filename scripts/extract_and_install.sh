358758c7e6f7a9ef77369f8a25c939e7bb6a8480
Nicholas Ngetich <nicholas.ngetich@griffinglobaltech.com>
2025-07-15 18:17:18 +0300

    update app

--- scripts/extract_and_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /home/ec2-user/app
-
-# Install dependencies
-pip3 install -r requirements.txt
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,3 +4,6 @@ set -e
 # Install system packages only
 yum update -y
 yum install -y python3 python3-pip
+
+# Install dependencies
+pip3 install -r requirements.txt
