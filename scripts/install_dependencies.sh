3ff39598c9b93796cd1bdd246757ea9139c399ae
Nicholas Ngetich <nicholas.ngetich@griffinglobaltech.com>
2025-07-15 17:21:19 +0300

    update app

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-# Update system packages and install Python3/pip3 if not present
+# First, install required packages
 yum update -y
 yum install -y python3 python3-pip
 
-# Install Python dependencies
+# Create the directory (in case CodeDeploy hasn't done it yet)
+mkdir -p /home/ec2-user/app
+
+# Now safe to change directory
 cd /home/ec2-user/app
+
+# Install Python dependencies
 pip3 install -r requirements.txt
